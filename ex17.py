a3e20d9fb927f8cafe9c26a4838a96139b59bd12
Shaban <shabanxhemajli@live.com>
2022-08-26 13:01:23 +0200

    first commit

---
+++ ex17.py
@@ -0,0 +1,4 @@
+phrase_tobereplaced="A dogmatic dog buys dogecoin to become rich and buy hotdogs every day."
+replacement="cat"
+replace_theword=f"A dogmatic {replacement} buys dogecoin to become rich and buy hotdogs every day."
+print(replace_theword)
